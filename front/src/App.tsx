99134105820d9ea1d02aa9d712900b096ca5d353
Nico <2438538@ucc.edu.ar>
2026-03-04 19:01:19 -0300

    v3.8 - Frontend Roto 2

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -194,7 +194,7 @@ export default function App() {
 
   return (
     <main className="container">
-      <h1>Task Manager - SE ROMPIO</h1>
+      <h1>Task Manager - Esto no deberia estar </h1>
       <p className="subtitle">CRUD basico con titulo, descripcion, edicion y fecha de creacion.</p>
 
       <form onSubmit={onSubmit} className="create-form">
